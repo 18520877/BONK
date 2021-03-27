b487069fa885aceb5359d76640dd6c1f2495b7cc
18521143 <79905379+18521143@users.noreply.github.com>
2021-03-28 00:51:29 +0700

    Add files via upload

---
+++ ASSIGNMENTS/WEEK 2/RTL/Src/rgb_to_gray.v
@@ -0,0 +1,32 @@
+module rgb_to_gray(R,G,B,clk,reset,invalid,Y,outvalid);
+  input [7:0] R,G,B;
+  input clk,reset,invalid;
+  
+  output [31:0] Y;
+  output outvalid;
+  
+  wire i2pEn,mulEn,addEn,f2iEn;
+  
+  datapath inst0( .R(R), 
+                  .G(G), 
+                  .B(B), 
+                  .clk(clk), 
+                  .i2pEn(i2pEn), 
+                  .mulEn(mulEn), 
+                  .addEn(addEn), 
+                  .f2iEn(f2iEn), 
+                  .Y(Y)
+                  );
+                  
+  controller inst1(.clk(clk),
+                   .reset(reset),
+						 .invalid(invalid),
+                   .i2pEn(i2pEn),
+                   .mulEn(mulEn),
+                   .addEn(addEn),
+                   .f2iEn(f2iEn),
+						 .outvalid(outvalid)
+              );
+endmodule
+
+
