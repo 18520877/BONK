28389e5ac39604337ae9da069c6e0d83404128b9
18520877 <79899590+18520877@users.noreply.github.com>
2021-07-25 20:00:01 +0700

    Delete npy2txt.py

--- PROJECT/SOURCES/Weight/npy2txt.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# data = np.load("Conv2d_7b_1x1_kernel.npy")
-# data = data.flatten()
-# np.savetxt("Conv2d_7b_1x1_kernel.txt", data, delimiter= "\n")
-
-# data = np.load("Logits_kernel.npy")
-# data = data.flatten()
-# np.savetxt("Logits_kernel.txt", data, delimiter= "\n")
