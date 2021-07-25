9da159cb4f7077a6d72cb6b9c8e4127e3885d951
18520877 <79899590+18520877@users.noreply.github.com>
2021-07-25 20:01:35 +0700

    Add files via upload

---
+++ PROJECT/SOURCES/WEIGHT/npy2txt.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# data = np.load("Conv2d_7b_1x1_kernel.npy")
+# data = data.flatten()
+# np.savetxt("Conv2d_7b_1x1_kernel.txt", data, delimiter= "\n")
+
+# data = np.load("Logits_kernel.npy")
+# data = data.flatten()
+# np.savetxt("Logits_kernel.txt", data, delimiter= "\n")
